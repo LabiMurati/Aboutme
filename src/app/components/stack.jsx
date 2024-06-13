e01a6a6cfe91d4db21e058d3033f230422832880
LabiMurati <67820477+LabiMurati@users.noreply.github.com>
2024-06-13 11:43:31 +0200

    remove text icons

--- src/app/components/stack.jsx
+++ src/app/components/stack.jsx
@@ -9,10 +9,9 @@ import devIcon from "../Images/dev.png";
 import tailIcon from "../Images/tail.svg";
 import githubIcon from "../Images/github.svg";
 
-const StackItem = ({ icon, title, description }) => (
+const StackItem = ({ icon,  description }) => (
   <div className="stack__item flex flex-col items-center p-4 border-2 border-gray-500 rounded-lg text-center" data-aos="flip-left" data-aos-duration="1000">
     <div className="stack__icon mb-2">{icon}</div>
-    <h3 className="stack__item-title text-xl font-medium mb-1">{title}</h3>
     <p className="stack__item-description text-gray-500">{description}</p>
   </div>
 );
@@ -21,18 +20,18 @@ const Stack = () => {
   const [showAll, setShowAll] = useState(false);
 
   const stackItems = [
-    { icon: <FaHtml5 size={30} />, title: "HTML5", description: "Markup Language" },
-    { icon: <FaCss3Alt size={30} />, title: "CSS3", description: "Style Sheet Language" },
-    { icon: <FaSass size={30} />, title: "Sass/SCSS", description: "CSS Preprocessor" },
-    { icon: <FaBootstrap size={30} />, title: "Bootstrap", description: "CSS Framework" },
-    { icon: <Image src={tailIcon} alt="Tailwind CSS Logo" width={30} />, title: "Tailwind CSS", description: "CSS Framework" },
-    { icon: <SiJquery size={30} />, title: "jQuery", description: "JavaScript Library" },
-    { icon: <FaJs size={30} />, title: "JavaScript", description: "Programming Language" },
-    { icon: <FaReact size={30} />, title: "React", description: "JavaScript Library" },
-    { icon: <SiNextdotjs size={30} />, title: "Next.js", description: "React Framework" },
-    { icon: <FaWordpress size={30} />, title: "WordPress", description: "CMS" },
-    { icon: <Image src={githubIcon} alt="GitHub Logo" width={30} />, title: "Git", description: "GitHub" },
-    { icon: <FaFigma size={30} />, title: "Figma", description: "Figma UI/UX" },
+    { icon: <FaHtml5 size={30} />, title: "HTML5"},
+    { icon: <FaCss3Alt size={30} />, title: "CSS3" },
+    { icon: <FaSass size={30} />, title: "Sass/SCSS" },
+    { icon: <FaBootstrap size={30} />, title: "Bootstrap"},
+    { icon: <Image src={tailIcon} alt="Tailwind CSS Logo" width={30} />, title: "Tailwind CSS" },
+    { icon: <SiJquery size={30} />, title: "jQuery" },
+    { icon: <FaJs size={30} />, title: "JavaScript"},
+    { icon: <FaReact size={30} />, title: "React"},
+    { icon: <SiNextdotjs size={30} />, title: "Next.js"},
+    { icon: <FaWordpress size={30} />, title: "WordPress" },
+    { icon: <Image src={githubIcon} alt="GitHub Logo" width={30} />, title: "Git" },
+    { icon: <FaFigma size={30} />, title: "Figma" },
   ];
 
   useEffect(() => {
