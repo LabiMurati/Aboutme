8214b44753ebd2b1f9673557ed31aa71f224552e
LabiMurati <67820477+LabiMurati@users.noreply.github.com>
2024-06-13 12:32:44 +0200

    Favicon change & logo

---
+++ public/document.jsx
@@ -0,0 +1,21 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
